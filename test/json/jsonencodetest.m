9798399db49814295f3454c95b77061b6d65e627
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2020-06-18 14:47:50 +0200

    Test 1: encode logical and numeric scalars, NaN and Inf

---
+++ test/json/jsonencodetest.m
@@ -0,0 +1,20 @@
+% test jsonencode
+
+% Note: This script is intended to be a script-based unit test
+%       for MATLAB to test compatibility.  Don't break that!
+
+%% Test 1: encode logical and numeric scalars, NaN and Inf
+assert (isequal (jsonencode (true), 'true'));
+assert (isequal (jsonencode (50.025), '50.025'));
+assert (isequal (jsonencode (NaN), 'null'));
+assert (isequal (jsonencode (Inf), 'null'));
+assert (isequal (jsonencode (-Inf), 'null'));
+
+% Customized encoding of Nan, Inf, -Inf
+assert (isequal (jsonencode (NaN, 'ConvertInfAndNaN', true), 'null'));
+assert (isequal (jsonencode (Inf, 'ConvertInfAndNaN', true), 'null'));
+assert (isequal (jsonencode (-Inf, 'ConvertInfAndNaN', true), 'null'));
+
+assert (isequal (jsonencode (NaN, 'ConvertInfAndNaN', false), 'NaN'));
+assert (isequal (jsonencode (Inf, 'ConvertInfAndNaN', false), 'Infinity'));
+assert (isequal (jsonencode (-Inf, 'ConvertInfAndNaN', false), '-Infinity'));
