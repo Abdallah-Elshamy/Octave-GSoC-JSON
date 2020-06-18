ffa66d4d9d4b78ae2a07f4a1d9bb7f97dff5d820
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2020-06-18 22:59:14 +0200

    Test 3: encode numeric and logical arrays (with NaN and Inf)

--- test/json/jsonencodetest.m
+++ test/json/jsonencodetest.m
@@ -29,3 +29,31 @@ data = [[['foo'; 'bar']; ['foo'; 'bar']], [['foo'; 'bar']; ['foo'; 'bar']]];
 exp  = '["foofoo","barbar","foofoo","barbar"]';
 act  = jsonencode (data);
 assert (isequal (exp, act));
+
+%% Test 3: encode numeric and logical arrays (with NaN and Inf)
+% test simple vectors
+assert (isequal (jsonencode ([]), '[]'));
+assert (isequal (jsonencode ([1, 2, 3, 4]]), '[1,2,3,4]'));
+assert (isequal (jsonencode ([true; false; true]), '[true,false,true]'));
+
+% test arrays
+data = [1 NaN; 3 4];
+exp  = '[[1,null],[3,4]]';
+act  = jsonencode (data);
+assert (isequal (exp, act));
+
+data = cat (3, [NaN, 3; 5, Inf], [2, 4; -Inf, 8]);
+exp  = '[[[null,2],[3,4]],[[5,null],[null,8]]]';
+act  = jsonencode (data);
+assert (isequal (exp, act));
+
+% Customized encoding of Nan, Inf, -Inf
+data = cat (3, [1, NaN; 5, 7], [2, Inf; 6, -Inf]);
+exp  = '[[[1,Nan],[3,Infinity]],[[5,6],[7,-Infinity]]]';
+act  = jsonencode (data, 'ConvertInfAndNaN', false);
+assert (isequal (exp, act));
+
+data = [true false; true false; true false];
+exp  = '[[true,false],[true,false],[true,false]]';
+act  = jsonencode (data);
+assert (isequal (exp, act));
